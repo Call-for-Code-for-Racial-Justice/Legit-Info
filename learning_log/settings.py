661ae40b8a75df706cb4f039f47d5966f9b1959b
Tony Pearson <tpearson@us.ibm.com>
2020-07-30 21:04:13 -0700

    new requirements

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -80,12 +80,18 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'learning_logs',
+        'USER': 'fixuser',
+        'PASSWORD': 'delta.jack.anvil',
+        'HOST': 'mariadb',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
