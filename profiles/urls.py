92a5a827e83e6b51de877f4c8b808c1d62d12683
Tony Pearson <tpearson@us.ibm.com>
2020-08-03 20:27:08 -0700

    removing profiles temporarily

--- profiles/urls.py
+++
@@ -1,9 +0,0 @@
-"""Defines URL patterns for users"""
-
-from django.urls import path, include
-
-app_name = 'profiles'
-urlpatterns = [
-    # Include default auth urls.
-    path('', include('django.contrib.auth.urls')),
-    ]
