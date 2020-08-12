4890bfd7af9b09bcc4d97fb870e56d75a351a981
Jermaine Edwards <jedward2@us.ibm.com>
2020-08-11 22:33:10 -0500

    Fix admin css support in Docker

--- cfc_project/settings.py
+++ cfc_project/settings.py
@@ -153,8 +153,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "staticfiles"),
+]
 
 # My settings
 LOGIN_URL = 'users:login'
