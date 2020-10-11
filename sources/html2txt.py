2b296e1dda1e97b0a5b25af4f539bf260d621433
Tony Pearson <tpearson@us.ibm.com>
2020-10-11 05:35:20 -0700

    fix flake8 errors on html2txt.py

--- sources/html2txt.py
+++ sources/html2txt.py
@@ -2,13 +2,10 @@
 # html2txt.py -- Convert Legiscan.com HTML to TXT file
 # By Tony Pearson, IBM, 2020
 #
-import os
-import re
-import requests
 import sys
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
 PARSER = "lxml"
+
 
 def parse_html(htmlname):
     page = open(htmlname, "rb").read().decode('utf-8', 'ignore')
@@ -31,41 +28,6 @@ def parse_html(htmlname):
             print(pg, file=outfile)
 
     return None
-
-def parse(postname):
-    """ Parse the post to extract all links """
-    soup = BeautifulSoup(open(postname), PARSER)
-    problems = []
-    links = soup.select('a')
-    for link in links:
-        extlink = link.get('href')
-        display_problems = False
-
-        # Not all <a> tags have HREF links
-        if extlink is None:
-            display_problems = False
-        # All links to IBM developerWorks need to be corrected
-        elif extlink.startswith(DWORKS):
-            display_problems = True
-            code = 301
-        # Eliminate boilerplate links, allow other links to be investigated
-        elif allowlist(extlink):
-            code = 404
-            try:
-                res = requests.get(extlink, timeout=5)
-                code = res.status_code
-            except Exception:
-                code = 408
-            if (code != requests.codes.ok):
-                display_problems = True
-        if display_problems:
-            problems.append(str(code)+' '+extlink)
-
-    # If problems with links found, print postname and list of problems
-    if problems:
-        print(postname, '-- problems found:', len(problems), file=out_file)
-        for problem in problems:
-            print('   ', problem, file=out_file)
 
 
 def get_parms(argv):
@@ -91,9 +53,8 @@ if __name__ == "__main__":
     # Check proper input syntax
     display_help, htmlname = get_parms(sys.argv)
     if not display_help:
-        outname = htmlname.replace('.html','.txt')
+        outname = htmlname.replace('.html', '.txt')
         with open(outname, "w") as outfile:
             parse_html(htmlname)
-
 
     print("File {} created".format(outname))
