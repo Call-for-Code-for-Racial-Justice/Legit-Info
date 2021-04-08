00ec9eb969b22023011a0821ea6f4a939c2e2e8d
Tony Pearson <tpearson@us.ibm.com>
2021-04-08 16:01:21 -0700

    add dockerfile2 update weekly.sh

--- weekly.sh
+++ weekly.sh
@@ -3,6 +3,6 @@
 # Use cron1 for SQLite3, and cron2 for PostgreSQL.
 # --limit 0 means "unlimited" and will work on all files found
 
-/home/yourname/Dev/Legit-Info/cron1 get_datasets --api
-/home/yourname/Dev/Legit-Info/cron1 extract_files --api --skip --limit 0
-/home/yourname/Dev/Legit-Info/cron1 analyze_text --api --skip --compare --limit 0
+/app/cron1 get_datasets --api
+/app/cron1 extract_files --api --skip --limit 0
+/app/cron1 analyze_text --api --skip --compare --limit 0
