92363aedf78dedc51be0dc6af35ef5c65a702da6
Brodie <bzoschke@gmail.com>
2022-01-28 11:54:21 -0700

    Adding a windows batch file to act like the stage1 for other environments, updated readme to include the new batch file options
    Signed-off-by: Brodie <bzoschke@gmail.com>

---
+++ stage1.bat
@@ -0,0 +1,13 @@
+@ECHO off
+:: This script runs the application with SQLite3 (see STAGE1.md for details)
+::
+:: Examples:
+:: stage1.bat runserver localhost:3000   <--- default if nothing specified
+:: stage1.bat shell
+:: stage1.bat makemigrations
+:: stage1.bat migrate
+:: stage1.bat dbshell
+:: stage1.bat qcluster
+SET parms="%*"
+if [%parms%] == [""] ( SET parms=runserver localhost:3000 ) ELSE ( SET parms=%parms:"=%)
+python manage.py %parms%
