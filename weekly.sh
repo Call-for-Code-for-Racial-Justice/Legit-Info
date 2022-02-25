8c8f0876b96df9747850e61967b79b6a66fe6407
Shubham Saxena <s.shubham.work@gmail.com>
2022-02-25 17:26:42 +0530

    Add checks for FOB storage and Legiscan API Key env variables; convert absolute paths to relative in cron jobs
    Signed-off-by: Shubham Saxena <s.shubham.work@gmail.com>

--- weekly.sh
+++ weekly.sh
@@ -3,6 +3,18 @@
 # Use cron1 for SQLite3, and cron2 for PostgreSQL.
 # --limit 0 means "unlimited" and will work on all files found
 
-/app/cron1 get_datasets --api
-/app/cron1 extract_files --api --skip --limit 0
-/app/cron1 analyze_text --api --skip --compare --limit 0
+if [[ -z "${FOB_STORAGE}" ]]; then
+	echo "Error: FOB_STORAGE environment variable not found"
+	echo "Please set the FOB_STORAGE environment variable before running the script."
+	exit 0
+fi
+
+if [[ -z "${LEGISCAN_API_KEY}" ]]; then
+	echo "Error: LEGISCAN_API_KEY environment variable not found"
+	echo "Please set the LEGISCAN_API_KEY environment variable before running the script."
+	exit 0
+fi
+
+./cron1 get_datasets --api
+./cron1 extract_files --api --skip --limit 0
+./cron1 analyze_text --api --skip --compare --limit 0
