ba3a6908d3c4fbc1b197307b15d0b21cdfa46357
Charlie Evans <charlie.glenn.evans.jr@gmail.com>
2022-09-01 16:20:16 -0500

    Updated main Dockerfile and entrypoint script with Charles' changes
    Signed-off-by: Charlie Evans <charlie.glenn.evans.jr@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-# python3 manage.py migrate --no-input
-python3 manage.py collectstatic --no-input --clear
+if [[ ! -z ${POSTGRESQL_DATABASE_FILE} ]]; then
+  export POSTGRESQL_DATABASE=`cat $POSTGRESQL_DATABASE_FILE`
+fi
+if [[ ! -z ${POSTGRESQL_USER_FILE} ]]; then
+  export POSTGRESQL_USER=`cat $POSTGRESQL_USER_FILE`
+fi
+if [[ ! -z ${POSTGRESQL_PASSWORD_FILE} ]]; then
+  export POSTGRESQL_PASSWORD=`cat $POSTGRESQL_PASSWORD_FILE`
+fi
 
-exec "$@"
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py loaddata sources/cfc-seed.json
+
+TEST="${USE_SQLITE3:-False}"
+if [[ "${TEST,,}" == "true" ]]; then
+  python3 manage.py runserver 0.0.0.0:8080
+else
+  gunicorn -b 0.0.0.0:8080 --env DJANGO_SETTINGS_MODULE=cfc_project.settings cfc_project.wsgi --timeout 120
+fi
